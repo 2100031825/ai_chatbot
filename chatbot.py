e793b7bc17b3513a37ab718544d133afb12d2278
2100031825 <140370358+2100031825@users.noreply.github.com>
2024-09-29 16:48:21 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -21,18 +21,7 @@ def role_to_streamlit(role):
 for message in st.session_state.chat.history:
     with st.chat_message(role_to_streamlit(message.role)):
         st.markdown(message.parts[0].text)
-
-
-# uploaded_file = st.file_uploader("Upload a pdf, docx, or txt file",
-#                                 type=['png', 'jpg', 'jpeg', "pdf", "docx", "txt"])
-
-# if uploaded_file is not None:
-#     uploaded_file = Image.open(uploaded_file)
-#     model1 = genai.GenerativeModel('gemini-pro-vision')
-#     response = model1.generate_content(["Generate the output for the image",uploaded_file],stream=True)
-#     response.resolve()
-#     st.markdown(response.text)
-#     st.write(response.text)
+        
 
 if prompt := st.chat_input("I possess a well of knowledge. What would you like to know?"):
     st.chat_message("user").markdown(prompt)
@@ -41,17 +30,6 @@ if prompt := st.chat_input("I possess a well of knowledge. What would you like t
     with st.chat_message("assistant"):
         st.markdown(response.text)
 
-
-
-# model = genai.GenerativeModel('gemini-pro-vision')
-# response = model.generate_content(
-#     glm.Content(
-#         parts = [
-#             glm.Part(text="Write a short, engaging blog post based on this picture."),
-#             glm.Part(
-#                 inline_data=glm.Blob(
-#                     mime_type='image/jpeg',
-#                     data=pathlib.Path('image.jpg').read_bytes()
 #                 )
 #             ),
 #         ],
